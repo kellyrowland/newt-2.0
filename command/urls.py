4bd577432779dc9471da726b25bddb187eb30267
Kelly Rowland <kellylynnerowland@gmail.com>
2019-05-17 14:18:33 -0700

    update command urls

--- command/urls.py
+++ command/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from command.views import CommandView, CommandRootView, ExtraCommandView
 
 urlpatterns = [
-    url(r'^$', CommandRootView.as_view()),
-    url(r'^(?P<machine_name>[^/]+)$', CommandView.as_view()),
-    url(r'^(?P<query>.+)/$', ExtraCommandView.as_view()),
+    path(r'', CommandRootView.as_view()),
+    path(r'command/<slug:machine_name>/', CommandView.as_view()),
+    path(r'<str:query>/', ExtraCommandView.as_view()),
 ]
     
