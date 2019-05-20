07789ce385288716adc89610182bf52da1cece74
Kelly Rowland <kellylynnerowland@gmail.com>
2019-05-20 10:15:09 -0700

    update status urls and tests

--- status/tests.py
+++ status/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.conf import settings
+from django.urls import reverse
 import json
-from newt.tests import MyTestClient, newt_base_url, login
+from newt.tests import MyTestClient
 
 
 class StatusTests(TestCase):
@@ -9,7 +10,7 @@ class StatusTests(TestCase):
         self.client = MyTestClient()
 
     def test_all(self):
-        r = self.client.get(newt_base_url+'/status')
+        r = self.client.get(reverse('newt-status'))
         self.assertEqual(r.status_code, 200)
 
         json_response = r.json()
@@ -23,7 +24,7 @@ class StatusTests(TestCase):
 
     def test_one(self):
         system = settings.NEWT_CONFIG['SYSTEMS'][0]['NAME']
-        r = self.client.get('%s/status/%s' % (newt_base_url, system))
+        r = self.client.get(reverse('newt-status-machine',args=(system,)))
         self.assertEqual(r.status_code, 200)
 
         json_response = r.json()
--- status/urls.py
+++ status/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from status.views import StatusView, ExtraStatusView
 
 urlpatterns = [
-    path(r'', StatusView.as_view()),
-    path(r'status/<slug:machine_name>/', StatusView.as_view()),
+    path(r'', StatusView.as_view(), name='newt-status'),
+    path(r'<slug:machine_name>/', StatusView.as_view(), name='newt-status-machine'),
     path(r'<str:query>/', ExtraStatusView.as_view()),
 ]
     
