e7e3bca29655d76d5b860584ab86d71842322a76
Kelly Rowland <kellylynnerowland@gmail.com>
2019-05-20 13:58:18 -0700

    name authnz url

--- authnz/urls.py
+++ authnz/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from authnz.views import AuthView, ExtraAuthView
 
 urlpatterns = [
-    path('', AuthView.as_view()),
+    path('', AuthView.as_view(), name='newt-auth'),
     path('<str:query>', ExtraAuthView.as_view()),
 ]
     
