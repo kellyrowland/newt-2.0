54c278c7b3e4c04c35326df96ea5f61033ee7c52
Kelly Rowland <kellylynnerowland@gmail.com>
2019-05-21 10:36:57 -0700

    update store urls and tests

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from store.views import *
 
 urlpatterns = [
-    path(r'', StoreRootView.as_view()),
-    path(r'store/<slug:store_name>/', StoreView.as_view()),
-    path(r'store/<slug:store_name>/perms/', StorePermView.as_view()),
-    path(r'store/<slug:store_name>/<int:obj_id>/', StoreObjView.as_view()),
+    path(r'', StoreRootView.as_view(),name='newt-store'),
+    path(r'<slug:store_name>/', StoreView.as_view(), name='newt-store-name'),
+    path(r'<slug:store_name>/perms/', StorePermView.as_view(), name='newt-store-perms'),
+    path(r'<slug:store_name>/<int:obj_id>/', StoreObjView.as_view(), name='newt-store-name-id'),
     path(r'<str:query>/', ExtraStoreView.as_view()),
 ]
