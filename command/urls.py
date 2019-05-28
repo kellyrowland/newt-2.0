e336e7d54daaf0be919b63068703034bc9cad674
Kelly Rowland <kellylynnerowland@gmail.com>
2019-05-28 10:44:21 -0700

    update command tests and urls

--- command/urls.py
+++ command/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from command.views import CommandView, CommandRootView, ExtraCommandView
 
 urlpatterns = [
-    path(r'', CommandRootView.as_view()),
-    path(r'command/<slug:machine_name>/', CommandView.as_view()),
-    path(r'<str:query>/', ExtraCommandView.as_view()),
+    path('', CommandRootView.as_view(), name='newt-command'),
+    path('<slug:machine_name>/', CommandView.as_view(), name='newt-command-machine'),
+    path('<str:query>/', ExtraCommandView.as_view()),
 ]
     
